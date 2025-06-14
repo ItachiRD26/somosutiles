bd712e385de7e07673a644bfb2eaa9ce1af44ecf
Peter <garciajeter9@gmail.com~>
2025-06-14 01:06:52 -0400

    Fixed: Android Pack Error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.jeterdev.utiles',
-  appName: 'Utiles Escolar',
+  appName: 'Utiles Escolares',
   webDir: 'out',
 };
 
